ee49422b5e222d087d687ff45bb6abbf3328e985
Christopher Burke <burkechrisburke@gmail.com>
2019-12-10 22:17:01 -0800

    trk34init matches Color Computer OS9 1.00.00 (all-disassembled)

--- os9/build.sh
+++ os9/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+xasm -l -m/d0/defs,../defs -m../../defs,../defs -i. -i../defs -o../modules/trk34init.krn trk34init
 xasm -l -m/d0/defs,../defs -m../../defs,../defs -i. -i../defs -o../modules/os9p1.krn os9p1
 xasm -l -m/d0/defs,../defs -m../../defs,../defs -i. -i../defs -o../modules/os9p2.krn os9p2
 xasm -l -m/d0/defs,../defs -m../../defs,../defs -i. -i../defs -o../modules/sysgo.krn sysgo
