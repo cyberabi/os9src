9e5ef36525a39c19ee032488573267ac7b190f0d
Chris Burke <chris@cyberabi.com>
2020-01-22 23:57:34 -0800

    Commented on VIRQ support in edition 4

--- floppy/b.sh
+++ floppy/b.sh
@@ -2,6 +2,4 @@
 xasm -l -s -m/d0/defs,../defs -m../../defs,../defs -i. -i../defs -o../modules/CCDisk ccdisk
 xasm -l -s -m/d0/defs,../defs -m../../defs,../defs -i. -i../defs -o../modules/D0 d0
 xasm -l -s -m/d0/defs,../defs -m../../defs,../defs -i. -i../defs -o../modules/D1 d1
-xasm -l -s -m/d0/defs,../defs -m../../defs,../defs -i. -i../defs -o../modules/D2 d2
-xasm -l -s -m/d0/defs,../defs -m../../defs,../defs -i. -i../defs -o../modules/D3 d3
 
