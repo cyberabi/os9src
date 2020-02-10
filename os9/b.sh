69e7021878a6bcd457faf1ced0ca95556d8b2305
Christopher Burke <burkechrisburke@gmail.com>
2020-02-10 07:34:51 -0800

    Created L2V200 branch; initial work on OS9p1

--- os9/b.sh
+++ os9/b.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xasm -l -m/d0/defs,../defs -m../../defs,../defs -i. -i../defs -o../modules/boot.krn boot
+xasm -l -m/d0/defs,../defs -m../../defs,../defs -i. -i../defs -o../modules/OS9p1.krn OS9p1
 
