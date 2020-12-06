473e30d1af6350997d5efb8c0b5b55455a9627e0
Christopher Burke <burkechrisburke@gmail.com>
2020-12-06 00:18:16 -0800

    Progress toward boot matching OS9 Level II V2.0.0

--- os9/b.sh
+++ os9/b.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xasm -l -m/d0/defs,../defs -m../../defs,../defs -i. -i../defs -o../modules/OS9p2.krn OS9p2
+xasm -l -m/d0/defs,../defs -m../../defs,../defs -i. -i../defs -o../modules/boot.krn boot
 
