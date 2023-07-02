b1fdd4997ee283cbbff4af87c4a517f93ea9971a
Christopher Burke <chris@cyberabi.com>
2023-07-02 00:33:10 -0700

    Checking in work in progress

--- os9/b1.sh
+++ os9/b1.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 xasm -l -m/d0/defs,../defs -m../../defs,../defs -i. -i../defs -o../modules/$1.krn $1
-diff -bs ../modules/$1.krn ../../tandy/L2V200/dist/$1
+diff -bs ../modules/$1.krn <(cat ../../tandy/L2V200/dist/$1 ../../tandy/L2V200/dist/_vectors)
 
